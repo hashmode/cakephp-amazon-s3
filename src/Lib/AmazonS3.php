dfb7e86d0941d6620a7aa01ab6bb5d0dd7cabb8e
hashmode <hashmode@users.noreply.github.com>
2016-11-19 18:51:19 +0400

    Update AmazonS3.php

--- src/Lib/AmazonS3.php
+++ src/Lib/AmazonS3.php
@@ -4,6 +4,7 @@ namespace CakephpAmazonS3\Lib;
 use Cake\Core\Configure;
 use Cake\Core\Exception\Exception;
 use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
 
 /**
  * AmazonS3
@@ -50,6 +51,20 @@ class AmazonS3
      * @var \Aws\S3\S3Client
      */
     private $s3Client = null;
+    
+    
+    /**
+     * object permission in s3
+     * private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control 
+     * @link http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObject
+     */
+    const ACL_PRIVATE = 'private';
+    const ACL_PUBLIC_READ = 'public-read';
+    const ACL_PUBLIC_READ_WRITE = 'public-read-write';
+    const ACL_AUTHENTICATED_READ = 'authenticated-read';
+    const ACL_BUCKET_OWNER_READ = 'bucket-owner-read';
+    const ACL_BUCKET_OWNER_FULL_CONTROL = 'bucket-owner-full-control';
+    
 
     /**
      *
@@ -90,34 +105,42 @@ class AmazonS3
      * @param string $localFile
      * @param string $remoteFile
      * @param string $permission
-     * @param array $options - all the rest options by
+     * @param array $headers - all the rest options by
      *         @link http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObject
      * @return boolean
      */
-    public function putObject($localFile, $remoteFile, $permission = null, $options = [])
+    public function putObject($localFile, $remoteFile, $permission = null, $headers = [])
     {
         if (!file_exists($localFile)) {
             return false;
+        }
+        
+        if ($permission === null) {
+            $permission = self::ACL_PRIVATE;
         }
         
         $args = [
             'Bucket' => $this->bucket,
             'Key' => $remoteFile,
             'Body' => fopen($localFile, 'r'),
-            'ACL' => 'public-read'
+            'ACL' => $permission
         ];
 
-        if (!empty($options)) {
-            foreach ($options as $key => $value) {
+        if (!empty($headers)) {
+            foreach ($headers as $key => $value) {
                 $args[$key] = $value;
             }
         }
 
-        $result = $this->s3Client->putObject($args);
-        $metadata = $result->get('@metadata');
-        
-        if (!empty($metadata['statusCode']) && $metadata['statusCode'] == 200) {
-            return true;
+        try {
+            $result = $this->s3Client->putObject($args);
+            $metadata = $result->get('@metadata');
+            
+            if (!empty($metadata['statusCode']) && $metadata['statusCode'] == 200) {
+                return true;
+            }
+        } catch (S3Exception $e) {
+            
         }
         
         return false;
