6da7cb34dec35eca1ef8205f5064ee926e7222fb
hashmode <hashmode@users.noreply.github.com>
2016-11-20 13:12:50 +0400

    Update AmazonS3.php

--- src/Lib/AmazonS3.php
+++ src/Lib/AmazonS3.php
@@ -13,6 +13,12 @@ use Aws\S3\Exception\S3Exception;
 class AmazonS3
 {
 
+    /** 
+     * api version to use
+     * @var string
+     */
+    private $version = '2006-03-01';
+    
     /**
      * S3 key
      * 
@@ -55,17 +61,24 @@ class AmazonS3
     
     /**
      * object permission in s3
-     * private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control 
-     * @link http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObject
+     * private|public-read|public-read-write|authenticated-read|aws-exec-read|bucket-owner-read|bucket-owner-full-control 
+     * @link http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-s3-2006-03-01.html#putobject
      */
     const ACL_PRIVATE = 'private';
     const ACL_PUBLIC_READ = 'public-read';
     const ACL_PUBLIC_READ_WRITE = 'public-read-write';
     const ACL_AUTHENTICATED_READ = 'authenticated-read';
+    const ACL_AWS_EXEC_READ = 'aws-exec-read';
     const ACL_BUCKET_OWNER_READ = 'bucket-owner-read';
     const ACL_BUCKET_OWNER_FULL_CONTROL = 'bucket-owner-full-control';
     
+    /** 
+     * default permission for objects created in s3
+     * @var string
+     */
+    private $permission = self::ACL_PRIVATE;
 
+    
     /**
      *
      * {@inheritDoc}
@@ -97,33 +110,30 @@ class AmazonS3
             ],
             'endpoint' => $this->endpoint,
             'region' => $this->region,
-            'version' => 'latest'
+            'version' => $this->version
         ]);
     }
 
     /**
-     * @param string $localFile
-     * @param string $remoteFile
+     * @param string $localFile - full path to file to be uploaded
+     * @param string $remoteFile - file "path" in s3 - relative the bucket name (without starting slash)
      * @param string $permission
      * @param array $headers - all the rest options by
-     *         @link http://docs.aws.amazon.com/aws-sdk-php/v2/api/class-Aws.S3.S3Client.html#_putObject
+     *         @link http://docs.aws.amazon.com/aws-sdk-php/v3/api/api-s3-2006-03-01.html#putobject
      * @return boolean
      */
     public function putObject($localFile, $remoteFile, $permission = null, $headers = [])
     {
-        if (!file_exists($localFile)) {
+        $localFile = trim($localFile);
+        if ($localFile != '' && !file_exists($localFile)) {
             return false;
-        }
-        
-        if ($permission === null) {
-            $permission = self::ACL_PRIVATE;
         }
         
         $args = [
             'Bucket' => $this->bucket,
             'Key' => $remoteFile,
-            'Body' => fopen($localFile, 'r'),
-            'ACL' => $permission
+            'Body' => $localFile == '' ? '' : fopen($localFile, 'r'),
+            'ACL' => $this->getPermission($permission)
         ];
 
         if (!empty($headers)) {
@@ -147,6 +157,99 @@ class AmazonS3
     }
     
     
+    /**
+     * listObjects method
+     * returns the list of object in the given path
+     * 
+     * @param string $path
+     * @return \Aws\Result|boolean
+     */
+    public function listObjects($path)
+    {
+        try {
+            $result = $this->s3Client->listObjects([
+                'Bucket' => $this->bucket,
+                'Marker' => $path
+            ]);
+            
+            $metadata = $result->get('@metadata');
+            
+            if (!empty($metadata['statusCode']) && $metadata['statusCode'] == 200) {
+                $items = $result->get('Contents');
+                return $items;
+            }
+        } catch (S3Exception $e) {
+            
+        }
+        
+        return false;
+    }
+
+    
+    public function getObjectUrl($path, $time = '+2 minutes')
+    {
+        $cmd = $this->s3Client->getCommand('GetObject', [
+            'Bucket' => $this->bucket,
+            'Key'    => $path
+        ]);
+        
+        $request = $this->s3Client->createPresignedRequest($cmd, $time);
+        
+        try {
+            // Get the actual presigned-url
+            $presignedUrl = (string) $request->getUri();        
+            
+            if (is_string($presignedUrl)) {
+                return $presignedUrl;
+            }
+        } catch (Exception $e) {
+        }
+
+        return false;
+    }
+    
+    
+    /**
+     * Creates folder (AKA prefix) in S3
+     * 
+     * @param string $path
+     * @param string $permission
+     * @return boolean
+     */
+    public function createFolder($path, $permission = null)
+    {
+        $path = trim($path, '/').'/';
+        
+        try {
+            return $this->putObject('', $path, $permission);
+        } catch (S3Exception $e) {
+            
+        }
+    
+        return false;
+    }
+    
+    
+    /**
+     * validates and returns the permission
+     * 
+     * @param string $permission
+     */
+    private function getPermission($permission = null)
+    {
+        if ($permission === null) {
+            return $this->permission;
+        }
+
+        $refl = new \ReflectionClass($this);
+        $constants = $refl->getConstants();
+        
+        if (in_array($permission, $constants)) {
+            return $permission;
+        }
+        
+        return $this->permission;
+    }
     
     
     
